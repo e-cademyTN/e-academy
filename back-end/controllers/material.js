dd14dabad1c53def61427afbe5456d8fcc55de15
OumaymaMansour <mansour.oumeima@gmail.com>
2024-03-20 23:29:06 +0100

    making controller for material

--- back-end/controllers/material.js
+++ back-end/controllers/material.js
@@ -2,23 +2,61 @@ const {Material} = require("../model")
 module.exports = {
   //admin/user
   getAllMaterial: async (req, res) => {
-   
+    try {
+      const result = await Material.findAll();
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(404).send(err);
+    }
   },
    ///user
   getOne: async (req, res) => {
-   
+    try {
+      const { id } = req.params;
+      const result = await Material.findByPk(id);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(404).send(error);
+    }
 },
  //admin
   createMaterial: async (req, res) => {
-    
+    try {
+      const {name, description,price,date,link} = req.body;
+      const result = await Material.create({ name, description,price,date,link });
+      res.status(201).json(result);
+    } catch (err) {
+      res.status(404).send(err);
+    }
 
   },
    //admin
   updateMaterial: async (req, res) => {
-    
+    try {
+      const {name,description,price,date,link} = req.body;
+      const result = await Material.update(
+        {
+          name,
+          description,
+          price,
+          date,
+          link
+        },
+        { where: { id: id } }
+      );
+      res.status(202).json(result);
+    } catch (err) {
+      res.status(404).send(err);
+    }
 
   }, //admin
   deleteMaterial: async (req, res) => {
-   
+    try {
+      const { id } = req.params;
+      const result = await Material.destroy({ where: { id: id } });
+      res.status(204).json(result);
+    } catch (err) {
+      res.status(404).send(err);
+    }
   }
 };
