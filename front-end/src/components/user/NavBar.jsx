a9b1a35c9bc97b5a84982eb1785807a36900e50e
Omarboubaker00 <omar.boubakker00@gmail.com>
2024-03-22 12:52:19 +0100

    styling the matirials

--- front-end/src/components/user/NavBar.jsx
+++ front-end/src/components/user/NavBar.jsx
@@ -1,33 +1,33 @@
 import React from 'react'
- import { useNavigate } from 'react-router-dom';
-import  './navBar.css'
+import { useNavigate } from 'react-router-dom';
+import './navBar.css'
 
 export const NavBar = () => {
-    const navigate = useNavigate();
-    return (
-        <nav className="navbar">
-          <div className="container">
-            <div className="navbar-brand">
-              <a className="navbar-item" href="/">
-                Your Logo
-              </a>
-            </div>
-            <div className="navbar-menu">
-              <div className="navbar-end">
-                <a onClick={()=>{navigate('/myMatiriels')}} className="navbar-item">
-                  My Matiriels
-                </a>
-                <a className="navbar-item" 
-                onClick={()=>{navigate('/editProfile')}}
-                >
-                  Edit Profile
-                </a>
-                <a className="navbar-item">
-                  Contact
-                </a>
-              </div>
-            </div>
+  const navigate = useNavigate();
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="/">
+            Your Logo
+          </a>
+        </div>
+        <div className="navbar-menu">
+          <div className="navbar-end">
+            <a onClick={() => { navigate('/myMatiriels') }} className="navbar-item">
+              My Matiriels
+            </a>
+            <a className="navbar-item"
+              onClick={() => { navigate('/editProfile') }}
+            >
+              Edit Profile
+            </a>
+            <a className="navbar-item">
+              Contact
+            </a>
           </div>
-        </nav>
-      );
+        </div>
+      </div>
+    </nav>
+  );
 }
