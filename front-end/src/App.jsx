8a231a8761aed8485d3952862d91b2028cca53e8
Omarboubaker00 <omar.boubakker00@gmail.com>
2024-03-22 13:30:37 +0100

    create materialDetail component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,21 +1,25 @@
 import { Route, Routes, Navigate } from "react-router-dom";
+import { useState } from "react";
 import Signup from ".//components/Singup/index.jsx";
 import Login from "./components/Login/index.jsx";
 import { Accueil } from "./components/user/Accueil.jsx";
 import MyMatiriels from "./components/user/MyMatiriels.jsx";
 import { EditProfile } from "./components/user/EditProfile.jsx";
+import { MatirialDetail } from "./components/user/MatirialDetail.jsx";
 
 function App() {
 	const user = localStorage.getItem("token");
+    const [selectedmat, setmat] = useState({})
 
 	return (
 		<Routes>
 			
 			<Route path="/signup"  element={<Signup />} />
 			<Route path="/login"  element={<Login />} />
-			<Route path="/userHome"  element={<Accueil />} />
+			<Route path="/userHome"  element={<Accueil setmat={setmat} />} />
 			<Route path="/myMatiriels"  element={<MyMatiriels />} />
 			<Route path="/editProfile"  element={<EditProfile />} />
+			<Route path="/matirialDetail"  element={<MatirialDetail selectedmat={selectedmat} />} />
 			
 			
 			<Route path="/" element={<Navigate replace to="/login" />} />
