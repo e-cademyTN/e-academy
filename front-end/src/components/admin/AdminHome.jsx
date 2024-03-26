ed52a6786d37222cf91386ae71aa742efc0bc79f
ahmedGDI <guedria.guedri@gmail.com>
2024-03-26 21:55:00 +0100

    add static statistic about how many student & material we've got in admin home page

--- front-end/src/components/admin/AdminHome.jsx
+++ front-end/src/components/admin/AdminHome.jsx
@@ -1,36 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from '../NavBar';
 import img from '../img/undraw_engineering_team_a7n2 (1).svg';
 import "../admin/homeCss.css";
 import { useNavigate } from 'react-router-dom';
+import axios from '../../assets/axiosSingleton.js';
 
 function AdminHome() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [studentCount, setStudentCount] = useState(0);
+  const [materialCount, setMaterialCount] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const [studentResponse, materialResponse] = await Promise.all([
+          axios.get('http://localhost:3000/api/users/getAll'),
+          axios.get('http://localhost:3000/api/material/getAll')
+        ]);
+
+       
+        const studentCount = studentResponse.data.length;
+        const materialCount = materialResponse.data.length;
+
+        setStudentCount(studentCount);
+        setMaterialCount(materialCount);
+      } catch (error) {
+        console.error('Error fetching counts:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className='admin-container'>
       <NavBar />
       <div className='container-2'>
         <div className='content'>
           <h1 className='title'>Welcome</h1>
-
-
-        <ul>
-          <li>
-
-        <p className='paragraph'>Student Information Management:
-The system maintains a centralized database containing detailed
- profiles of students, including personal information, contact details,
-  academic records, attendance, and disciplinary history.</p>
-          </li>
-        </ul>
-          
+          <ul>
+            <li>
+              <p className='paragraph'>Student Information Management: The system maintains a centralized database containing detailed profiles of students, including personal information, contact details, academic records, attendance, and disciplinary history.</p>
+            </li>
+          </ul>
+          <div className='totalCounts'>
+            <p>Total Students: <span>{studentCount}</span></p>
+            <p>Total Materials: <span>{materialCount}</span></p>
+          </div>
           <img className='image' src={img} alt="Engineering Team" />
-        <button
-        className='bttn'
-        onClick={()=>{
-            navigate("/materials")
-        }}
-        >get Start</button>
+          <button
+            className='bttn'
+            onClick={() => {
+              navigate("/materials");
+            }}
+          >Get Started</button>
         </div>
       </div>
     </div>
