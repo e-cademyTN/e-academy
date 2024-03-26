1aab7b0764f7360181173f08940468ebd82af44a
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-03-26 22:08:16 +0100

    setting the paths in the app

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,19 +1,25 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import Signup from "./components/Singup/index.jsx";
 import Login from "./components/Login/index.jsx";
-import Userhome from "./components/User/Userhome.jsx";
-import MyMaterials from "./components/User/MyMaterials.jsx";
-import { EditProfile } from "./components/User/EditProfile.jsx";
+import Userhome from "./components/user/Userhome.jsx";
+import MyMaterials from "./components/user/MyMaterials.jsx";
+import { EditProfile } from "./components/user/EditProfile.jsx";
 import AdminHome from "./components/admin/AdminHome.jsx";
 import Students from "./components/admin/Students.jsx";
 import Materials from "./components/admin/Materials.jsx";
-import UserMaterials from "./components/User/UserMaterials.jsx";
+import UserMaterials from "./components/user/UserMaterials.jsx";
 import CreateMat from "./components/admin/CreateMat.jsx";
 import UpdateMat from "./components/admin/UpdateMat.jsx";
+import UserMat from "./components/admin/UserMat.jsx";
+import ActivationPage from "./components/ActivationPage.jsx";
 
 function App() {
+  const token =localStorage.getItem('token')
   return (
-    <Routes>
+   ( <Routes>
+    < Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/confirm" element={<ActivationPage />} />
       <Route path="/createMat" element={<CreateMat />} />
       <Route path="/students" element={<Students />} />
       <Route path="/materials" element={<Materials />} />
@@ -21,12 +27,11 @@ function App() {
       <Route path="/updateprof" element={<EditProfile />} />
       <Route path="/mymaterial" element={<MyMaterials />} />
       <Route path="/userHome" element={<Userhome />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
       <Route path="/usermaterials" element={<UserMaterials />} />
       <Route path="/updatemat" element={<UpdateMat />} />
+      <Route path="/usermat" element={<UserMat />} />
       <Route path="/" element={<Navigate replace to="/login" />} />
-    </Routes>
+    </Routes>)
   );
 }
 
