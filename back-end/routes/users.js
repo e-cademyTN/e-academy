7e658e7ea9ad856baf4f0fb9994ab2a112e64827
Adem-abderrazek <Adem11.abderrazek@gmail.com>
2024-03-26 22:37:38 +0100

    setting user route

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -2,17 +2,17 @@ const express = require('express')
 const router = express.Router()
 
 
+
 const {verifyUser,signup,signin,getAllUsers,updateUser,getOne} = require('../controllers/users')
 
 const isAdminAuthenticated = require('../middlewares/isAdminAuthenticated')
 const isUserAuthenticated = require('../middlewares/isUserAuthenticated');
 
-
 router.get('/confirm/:activationcode', verifyUser);
 router.post('/signup',signup)
 router.post('/signin',signin)
 //admin
-router.get('/getAll',isAdminAuthenticated, getAllUsers)
+router.get('/getAll',isAdminAuthenticated,getAllUsers)
 //user
 router.put('/update/:id',isUserAuthenticated, updateUser)
 router.get("/getOne/:id",isUserAuthenticated,getOne)
