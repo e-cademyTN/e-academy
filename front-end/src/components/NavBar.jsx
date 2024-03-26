84544d2e0baaaf0df67e78080a15ae2e6ba6b473
OumaymaMansour <mansour.oumeima@gmail.com>
2024-03-26 14:31:31 +0100

    handling logout front

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -2,29 +2,31 @@ import React, { useState } from 'react';
 import "../../src/index.css";
 import {useNavigate} from 'react-router-dom';
 
+
 const Navbar = () => {
     const user=JSON.parse(localStorage.getItem("user"))
+
     const navigateTo =useNavigate()
+
     const [hover, setHover] = useState(false);
+
     const navUser = [
         { id: 1, text: 'Home', link: '/userHome' },
         { id: 2, text: 'My materials', link: '/mymaterial' },
-        { id: 3, text: 'Edit profile', link: '/updateprof' }
+        { id: 3, text: 'Edit profile', link: '/updateprof' },
+   
     ];
     const navAdmin = [
         { id: 1, text: 'Home', link: '/adminHome' },
         { id: 2, text: 'materials', link: '/materials' },
         { id: 3, text: 'Students', link: '/students' },
-        { id: 4, text: 'Edit profile', link: '/updateprof' }
+     
     ];
 
-
-
-    // const logout = () => {
-    //     localStorage.removeItem('x-token');
-    //     navigateto('/login')
-    // };
-
+   const logout =()=> {
+    localStorage.clear();
+    navigateTo('/login')
+}
     return (
         <div className='navbar'>
             <h1>E-Academy</h1>
@@ -39,6 +41,7 @@ const Navbar = () => {
                     </li>
                 ))}
             </ul>
+            <h1 onClick={()=>logout()}>logout</h1>
         </div>
     );
 };
